8e574f5771f558bd3d161d1855ecbac3645509cc
Yash <yash.saxena1217@gmail.com>
2014-11-23 03:10:05 -0800

    access origin update

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000', 'https://pinpoint-ionic-mobile.herokuapp.com', 'https://www.pinpoint-mobile.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000', 'https://pinpoint-ionic.herokuapp.com/', 'https://pinpoint-ionic-mobile.herokuapp.com', 'https://www.pinpoint-mobile.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
