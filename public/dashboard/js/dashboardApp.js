4eb45d876fcaa9164af35a6f7005451086c67c10
Yash <yash.saxena1217@gmail.com>
2015-01-31 19:37:16 -0800

    socket.io fix

--- public/dashboard/js/dashboardApp.js
+++ public/dashboard/js/dashboardApp.js
@@ -1,5 +1,17 @@
 var dashboardApp = angular.module('dashboardApp', ['ngRoute', 'ngAnimate', 'ngStorage']);
 'use strict';
+dashboardApp.factory('socket', function($scope) {
+  console.log('###############################')
+  // if ($scope.env === 'development') {
+  //   console.log('#### socket.io in development');
+  //   var socket = io.connect('http://localhost:3000');
+  //   return socket;
+  // } else {
+    var socket = io.connect('http://desolate-meadow-6374.herokuapp.com');
+    return socket;
+});
+
+
 dashboardApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: '/dashboard/views/scrapers.html',
@@ -14,14 +26,3 @@ dashboardApp.config(['$routeProvider', function($routeProvider) {
     controller: 'registerController'
   });
 }]);
-
-dashboardApp.factory('socket', function($scope) {
-  if ($scope.env === 'development') {
-    console.log('#### socket.io in development');
-    var socket = io.connect('http://localhost:3000');
-    return socket;
-  } else {
-    var socket = io.connect('https://desolate-meadow-6374.herokuapp.com');
-    return socket;
-  }
-});
