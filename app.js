70918f35abb45c3de6020ad7c531ac91750f2c36
Yash <yash.saxena1217@gmail.com>
2015-01-31 18:49:35 -0800

    production settings

--- app.js
+++ app.js
@@ -78,9 +78,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   // Production
-  // res.setHeader('Access-Control-Allow-Origin', 'https://pinpoint-ionic.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://pinpoint-ionic.herokuapp.com');
   // Development
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
